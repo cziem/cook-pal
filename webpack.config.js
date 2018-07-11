66a11807d7386e059326a98830e8393b3cb1051f
Fay Vor <phavorsparks@gmail.com>
2018-07-11 16:54:31 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: ["babel-polyfill", "./src/index.js"],
+    details: ["babel-polyfill", "./src/details.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "public/scripts"),
+    filename: "[name]-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+            plugin: ["transform-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/scripts/"
+  },
+  devtool: "source-map"
+};
