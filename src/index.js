c0161dbdc4107617858b72e320c4855c554f4502
Fay Vor <phavorsparks@gmail.com>
2018-07-12 15:27:41 +0100

    Add a call to renderRecipes and setup listener for double windows listenr

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import { setFilters } from "./filters";
 import { createRecipe } from "./recipes";
+import { renderRecipes } from "./views";
+
+renderRecipes();
 
 // setup listener for the search input
 document.querySelector("#search").addEventListener("input", e => {
   setFilters({ searchText: e.target.value });
-  // call renderRecipes
+  renderRecipes();
 });
 
 // setup listener for the sort input
 document.querySelector("#filter_by").addEventListener("change", e => {
   setFilters({ sortBy: e.target.value });
-  // call renderRecipes
+  renderRecipes();
 });
 
 // setup listener for addRecipe
 document.querySelector("#addRecipe").addEventListener("click", () => {
   const id = createRecipe();
   location.assign(`/details.html#${id}`);
+});
+
+// setup storage listener
+window.addEventListener("storage", e => {
+  if (e.key === "recipes") renderRecipes();
 });
