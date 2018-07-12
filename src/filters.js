732d1aa4d4f0dac416c744580c15bf7e9049726e
Fay Vor <phavorsparks@gmail.com>
2018-07-12 09:23:31 +0100

    Setup filter functions

--- src/filters.js
+++ src/filters.js
@@ -1,5 +1,17 @@
 const filters = {
   sortBy: 'byEdited',
-  searchText: ''
+  searchText: '',
+  hasIngredient: false
 }
 
+const getFilters = () => filters
+
+const setFilters = ({ sortBy, searchText, hasIngredient }) => {
+  if (typeof searchText === 'string') filters.searchText = searchText
+
+  if (typeof sortBy === 'string') filters.sortBy = sortBy
+
+  if (typeof hasIngredient === 'boolean') filters.hasIngredient = hasIngredient
+}
+
+export { getFilters, setFilters }
